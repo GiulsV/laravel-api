6dfd4b89ea28fcf3570845603377758295f557dc
GiuliaV <valente.giulia93@gmail.com>
2022-07-28 16:45:49 +0200

    modifica body guest/home.blade

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -64,26 +64,8 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ route('admin.dashboard') }}">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Boolpress (coming soon...)
-                </div>
-            </div>
-        </div>
+        <div id="root"></div>
+    
+        <script src="{{ asset('js/front.js') }}"></script>
     </body>
 </html>
